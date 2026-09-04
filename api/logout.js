77f48ac345e894e4d75f29e5d5251f447f6927c5
Francesco Leone Cocconi <escuelitadetecnologiatechyt367@gmail.com>
2026-09-04 09:59:07 -0300

    Agregando logout

--- api/logout.js
+++ api/logout.js
@@ -1,91 +1,71 @@
-module.exports =
-async (req, res) => {
-const origin =
-  req.headers.origin;
+module.exports = async (req, res) => {
 
-// =========================
-// CORS
-// =========================
+  const origin =
+    req.headers.origin;
 
-if (
-  origin ===
-    "http://localhost:8787" ||
-  origin ===
-    "http://127.0.0.1:8787"
-) {
+  if (
+    origin === "http://localhost:8787" ||
+    origin === "http://127.0.0.1:8787"
+  ) {
+
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      origin
+    );
+
+  }
 
   res.setHeader(
-    "Access-Control-Allow-Origin",
-    origin
+    "Access-Control-Allow-Credentials",
+    "true"
   );
 
-}
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
 
-res.setHeader(
-  "Access-Control-Allow-Credentials",
-  "true"
-);
-
-res.setHeader(
-  "Access-Control-Allow-Headers",
-  "Content-Type"
-);
-
-res.setHeader(
-  "Access-Control-Allow-Methods",
-  "POST, OPTIONS"
-);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS"
+  );
 
 
-// =========================
-// PREFLIGHT
-// =========================
+  if (
+    req.method === "OPTIONS"
+  ) {
 
-if (
-  req.method ===
-  "OPTIONS"
-) {
+    return res
+      .status(200)
+      .end();
+
+  }
+
+
+  if (
+    req.method !== "POST"
+  ) {
+
+    return res
+      .status(405)
+      .json({
+        error:
+          "Método no permitido"
+      });
+
+  }
+
+
+  res.setHeader(
+    "Set-Cookie",
+    "moonai_token=; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=0"
+  );
+
 
   return res
     .status(200)
-    .end();
-
-}
-
-
-// =========================
-// MÉTODO
-// =========================
-
-if (
-  req.method !==
-  "POST"
-) {
-
-  return res
-    .status(405)
     .json({
-      error:
-        "Método no permitido"
+      ok: true
     });
-
-}
-
-
-// =========================
-// BORRAR COOKIE
-// =========================
-
-res.setHeader(
-  "Set-Cookie",
-  "moonai_token=; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=0"
-);
-
-
-return res
-  .status(200)
-  .json({
-    ok: true
-  });
 
 };
